914d9a88a7d97f7b301b491aa7b77496678328dc
Andressa-Madureira <andressa.olmadureira@gmail.com>
2025-11-22 23:49:45 -0300

    feat: adiciona primeira atividade de Python

---
+++ atividade_pratica_1.py
@@ -0,0 +1,21 @@
+#Primeira atividade prática
+print('Escola de Dados da Alura')
+
+nome = input('Qual é o seu nome?')
+sobrenome = input('Qual é o seu sobrenome')
+
+print(f'Nome:{nome} \nSobrenome:{sobrenome}')
+for i in nome:
+    print(i)
+
+print('Por favor, informe sua data de nascimento')
+
+dia = input('Qual é o dia do seu nascimento?')
+mes = input('Qual é o mês do seu nascimento?')
+ano = input('Qual é o ano do seu nascimento?')
+
+print(f'{dia} {mes} {ano}')
+
+ano = input('Qual é o ano atual que estou fazendo o curso?')
+
+print(f'Ano atual: {ano}')
