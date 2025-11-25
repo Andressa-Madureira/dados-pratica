969268d16ec28324ade5a43d816ea261ed948110
Andressa-Madureira <andressa.olmadureira@gmail.com>
2025-11-25 20:34:02 -0300

    feat: adicionar exercícios de operações básicas com entrada de dados (soma, subtração e multiplicação)

---
+++ pratica_calculadora_com_operadores.py
@@ -0,0 +1,29 @@
+#Crie um programa que solicite dois valores numéricos à pessoa usuária e imprima a soma dos dois valores.
+
+primeiro_valor = int(input("Por favor, digite o primeiro valor: "))
+segundo_valor = int(input("Por favor, digite o segundo valor: "))
+resultado_soma = primeiro_valor + segundo_valor
+print(f'A soma dos dois valores é: {resultado_soma}')
+
+#2.Crie um programa que solicite três valores numéricos à pessoa usuária e imprima a soma dos três valores.(os inputs para o 2 valores já estão acima)
+
+primeiro_valor_soma = int(input("Por favor, digite o primeiro valor: "))
+segundo_valor_soma = int(input("Por favor, digite o segundo valor: "))
+terceiro_valor_soma = int(input("Por favor, digite o terceiro valor: "))
+resultado_soma_tres_valores = primeiro_valor_soma + segundo_valor_soma + terceiro_valor_soma
+print(f'A soma dos três valores é: {resultado_soma_tres_valores}')
+
+#3. Crie um programa que solicite dois valores numéricos à pessoa usuária e imprima a subtração do primeiro pelo o segundo valor.
+
+primeiro_valor_n1 = int(input('Por favor, digite o primeiro valor: '))
+segundo_valor_n2 = int(input('Por favor, digite o segundo valor: '))
+resultado_subtracao = primeiro_valor_n1 - segundo_valor_n2
+print(f'O valor da diferença é: {resultado_subtracao}')
+
+#4. Crie um programa que solicite dois valores numéricos à pessoa usuária e imprima a multiplicação dos dois valores.
+primeiro_valor_multi = int(input('Por favor, digite o primeiro valor: '))
+segundo_valor_multi = int(input('Por favor, digite o segundo valor: '))
+resultado_multi = primeiro_valor_multi * segundo_valor_multi
+print(f'O resultado da multiplicação é: {resultado_multi}')
+
+#5 - Continuar 
