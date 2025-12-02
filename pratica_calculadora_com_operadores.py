4507581613b4317469eb3002b03797128cc1b535
Andressa-Madureira <andressa.olmadureira@gmail.com>
2025-12-02 14:59:41 -0300

    feat: adicionar exercícios avançados de entrada de dados e operações matemáticas com tratamento de exceções

--- pratica_calculadora_com_operadores.py
+++ pratica_calculadora_com_operadores.py
@@ -26,4 +26,95 @@ segundo_valor_multi = int(input('Por favor, digite o segundo valor: '))
 resultado_multi = primeiro_valor_multi * segundo_valor_multi
 print(f'O resultado da multiplicação é: {resultado_multi}')
 
-#5 - Continuar 
+#5 - Crie um programa que solicite dois valores numéricos, um numerador e um denominador, e realize a divisão entre os dois valores. Deixe claro que o valor do denominador não pode ser 0.
+#Resultado com o que aprendi com o tratamento das exceções
+def calcular_divisao_segura():
+    while True:
+        try:
+            numerador = int(input('Por favor, digite o numerador: '))
+            denominador = int(input('Por favor, digite o denominador.(Não pode ser o 0 e precisa ser um número inteiro: ) '))
+            resultado_valor_divisao = numerador / denominador
+            print(f'O resultado é {resultado_valor_divisao}')
+            break
+
+        except ZeroDivisionError:
+            print('Atenção o denominador não pode ser 0. Tente novamente')
+        except ValueError:
+            print('Atenção. Você precisa digitar um número inteiro. Tente novamente')
+if __name__ == "__main__":
+    calcular_divisao_segura()
+        
+#Resultado sem utilizar o tratamento das execeções
+numerador = int(input('Por favor, digite o numerador: '))
+denominador = int(input('Por favor, digite o denominador.(Não pode ser o 0 e precisa ser um número inteiro: ) '))
+
+while denominador == 0:
+    print('Você digitou o 0. ')
+    denominador = int(input('Por favor, digite o denominador.(Não pode ser o 0 e precisa ser um número inteiro: ) '))
+resultado_valor_divisao = numerador / denominador
+print(f'O resultado é {resultado_valor_divisao}')
+
+#6. Crie um programa que solicite dois valores numéricos, um operador e uma potência, e realize a exponenciação entre esses dois valores.
+
+def calcular_potencia():
+    while True:
+        try:
+            operador = int(input('Por favor, digite o 1° número: '))
+            potencia = int(input('Por favor, digite o 2° número: '))
+            resultado_potencia = operador ** potencia
+            print(f'O resultado da potência é: {resultado_potencia}')
+            break
+        except ValueError:
+            print('Atenção! Você precisa digitar um número inteiro. Tente novamente.')
+if __name__ == "__main__":
+    calcular_potencia()
+
+#7. Crie um programa que solicite dois valores numéricos, um numerador e um denominador e realize a divisão inteira entre os dois valores. Deixe claro que o valor do denominador não pode ser 0.
+
+def calcular_divisao_inteira():
+    while True:
+        try:
+            numerador = int(input('Por favor, digite o numerador: '))
+            denominador = int(input('Por favor, digite o denominador(NÃO pode ser zero): '))
+            resultado_divisao_inteira = numerador // denominador
+            print(f'O resultado da divisão inteira é {resultado_divisao_inteira}')
+            break
+        except ValueError:
+            print('Valor inválido. Por favor, digite um número inteiro')
+        except ZeroDivisionError:
+            print('Atenção! O denominador não pode ser 0. Tente novamente')
+if __name__ == "__main__":
+    calcular_divisao_inteira()
+
+#8. Crie um programa que solicite dois valores numéricos, um numerador e um denominador, e retorne o resto da divisão entre os dois valores. Deixe claro que o valor do denominador não pode ser 0.
+
+def calcular_resto_divisao():
+    while True:
+        try:
+            numerador = int(input('Por favor, digite o numerador: '))
+            denominador = int(input('Por favor, digite o denominador(NÃO pode ser o zero): '))
+            resultado_resto_divisao = numerador % denominador 
+            print(f'O resultado do resto da divisão é: {resultado_resto_divisao}')
+            break
+        except ZeroDivisionError:
+            print('Atenção! O denominador não pode ser 0. Tente novamente')
+        except ValueError:
+            print('Valor inválido. Por favor, digite um número inteiro')
+if __name__ == "__main__":
+    calcular_resto_divisao()
+
+#9. Crie um código que solicita 3 notas de um estudante e imprima a média das notas.
+
+def calcular_media_notas():
+    while True:
+        try:
+            primeira_nota = float(input('Por favor, digite a sua 1° nota: '))
+            segunda_nota = float(input('Por favor, digite sua 2° nota: '))
+            terceira_nota = float(input('Por favor, digite sua 3° nota: '))
+            resultado_media = (primeira_nota + segunda_nota + terceira_nota) / 3
+            print(f'A média das notas é: {resultado_media:.2f}')
+            break
+        except ValueError:
+            print('Valor inválido! Por favor, digite um número real.')
+if __name__ == "__main__":
+    calcular_media_notas()
