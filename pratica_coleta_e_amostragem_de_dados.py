45ca0e5559bb4f9216ca8f1a12683bf05be76847
Andressa-Madureira <andressa.olmadureira@gmail.com>
2026-02-10 08:16:14 -0300

    feat: adiciona cálculo de divisão inteira com validações

--- pratica_coleta_e_amostragem_de_dados.py
+++ pratica_coleta_e_amostragem_de_dados.py
@@ -80,4 +80,18 @@ if __name__ == "__main__":
 def calcular_divisao_inteira():
      while True:
           try:
-               numerador
+               numerador = int(input("Por favor, digite o numerador: "))
+               denominador = int(input("Por favor, digite o denominador. Atenção! O denominador não pode ser o 0."))
+               resultado_div_int = numerador//denominador
+               print(f"O resultado da divisão inteira é: {resultado_div_int}")
+               break
+
+          except ValueError:
+               print("Atenção! Valor inválido! Tente novamente, por favor")
+          except ZeroDivisionError:
+               print("Atenção! O denominador não pode ser o 0. Tente novamente, por favor")
+if __name__ == "__main__":
+     calcular_divisao_inteira()
+
+
+#8)Crie um programa que solicite dois valores numéricos, um numerador e um denominador, e retorne o resto da divisão entre os dois valores. Deixe claro que o valor do denominador não pode ser 0.
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,15 +1,15 @@
-def potencia():
+def calcular_divisao_inteira():
      while True:
-          
           try:
-               operador = int(input('Por favor, digite o operador: '))
-               potencia = int(input('Por favor, digite uma potência'))
-               resultado_potencia = operador ** potencia
-               print(f'O resultado da potência é: {resultado_potencia}')
+               numerador = int(input("Por favor, digite o numerador: "))
+               denominador = int(input("Por favor, digite o denominador. Atenção! O denominador não pode ser o 0."))
+               resultado_div_int = numerador//denominador
+               print(f"O resultado da divisão inteira é: {resultado_div_int}")
                break
-          
-          except ValueError:
-               print('Por favor, digite um número! Informe os valores corretamente')
 
+          except ValueError:
+               print("Atenção! Valor inválido! Tente novamente, por favor")
+          except ZeroDivisionError:
+               print("Atenção! O denominador não pode ser o 0. Tente novamente, por favor")
 if __name__ == "__main__":
-     potencia()
+     calcular_divisao_inteira()
